26bc7aa6606ef14429dcbfbfd98325afc85ac46e
Takumi Kurogi <takumines6@gmail.com>
2020-04-20 17:51:02 +0900

    chapter6

--- todoapp/resources/lang/jp/validation.php
+++ todoapp/resources/lang/jp/validation.php
@@ -116,6 +116,7 @@ return [
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute format is invalid.',
     'uuid' => 'The :attribute must be a valid UUID.',
+    'date' => ':attribute には日付を入力してください。',
 
     /*
     |--------------------------------------------------------------------------
