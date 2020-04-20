9253af6cad48721a1a2a7706895ae597658f1bb3
Takumi Kurogi <takumines6@gmail.com>
2020-04-20 14:36:52 +0900

    chapter3

--- todoapp/config/database.php
+++ todoapp/config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'db-candy'),
+            'host' => env('DB_HOST', 'mysql'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'todoapp'),
             'username' => env('DB_USERNAME', 'default'),
--- todoapp/routes/web.php
+++ todoapp/routes/web.php
@@ -10,7 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
